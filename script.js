42f6c02054a693ee4a9bb6093228c21e27249cb1
alexandru233847 <alexutualexino33@gmail.com>
2024-09-30 17:32:37 +0200

    final price

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ let age = parseInt(prompt());
 
 // price km
 const priceKm = 0.21;
+let = finalPrice = km * priceKm;
 
+// age discount 
+if (age < 18) {
+    finalPrice *= 0.8;
+}
+
+// age over
+else if(age > 65) {
+    finalPrice *= 0.6;
+}
 
